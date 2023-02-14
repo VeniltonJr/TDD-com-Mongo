92ea2e348dd9b485778764af8f3d00eb8c4ae1e6
Venilton <venilton.junior@nimbus.house>
2023-02-14 15:27:04 -0300

    test: signUpController retorna 400 se não for fornecido email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('faltou preencher o campo: nome'))
   })
+
+  test('se o email não for informado retorna 400', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('faltou preencher o campo: email'))
+  })
 })
