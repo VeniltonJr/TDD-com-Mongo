f56fc35dc2d95b5e4e91f05da76cd155940aa86e
Venilton <venilton.junior@nimbus.house>
2023-02-15 09:24:52 -0300

    refactor: faz o SignUpController conforme um protocolo

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from '../errors/missingParamError'
 import { httpRequest, httpResponse } from '../protocols/http'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controllers'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: httpRequest): httpResponse {
     const requireFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requireFields) {
