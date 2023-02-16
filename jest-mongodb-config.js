462bb51086a3b530c63c3e1d5c0bdf7a5071b443
Venilton <venilton.junior@nimbus.house>
2023-02-16 13:57:38 -0300

    refactor: move o metodo do mongodb

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,13 +1,12 @@
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
-      version: '5.0.1',
+      version: '4.0.3',
       skipMD5: true,
     },
     instance: {
       dbName: 'jest',
     },
     autoStart: false,
-    instance: {},
   },
-};
+}
