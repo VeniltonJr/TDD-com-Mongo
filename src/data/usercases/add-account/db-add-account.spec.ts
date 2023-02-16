898e6b08b852519679adfaa9d42e24deb41a5fe0
Venilton <venilton.junior@nimbus.house>
2023-02-16 10:22:49 -0300

    test: garante que DbAddAccount retorna um account se tudo der certo

--- src/data/usercases/add-account/db-add-account.spec.ts
+++ src/data/usercases/add-account/db-add-account.spec.ts
@@ -83,7 +83,7 @@ describe('DbAddAccount Usercase', () => {
     })
   })
 
-  test('deve repassar a excecao que receber do encrypter', async () => {
+  test('deve repassar a excecao que receber do AddAccountRepositoryStub', async () => {
     const { sut, addAccountRepositoryStub } = makeSut()
     jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const accountData = {
@@ -93,5 +93,21 @@ describe('DbAddAccount Usercase', () => {
     }
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('deve retornar um account se tudo de certo', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_password'
+    })
   })
 })
