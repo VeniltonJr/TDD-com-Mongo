927723595b09ef60199bc5867f53b4add288d514
Venilton <venilton.junior@nimbus.house>
2023-02-17 10:50:19 -0300

    feat: conectando no mongodb antes do app escutar

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,8 +4,8 @@ import { AccountModel } from '../../../../domain/models/account'
 export const MongoHelper = {
   client: null as MongoClient,
 
-  async connect (uri: string) {
-    this.client = await MongoClient.connect(process.env.MONGO_URL)
+  async connect (url: string) {
+    this.client = await MongoClient.connect(url)
   },
   async disconnect (): Promise<void> {
     await this.client.close()
