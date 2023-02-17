52ba07d77808e0dfbaa7126122c8a2ad63eb907b
Venilton <venilton.junior@nimbus.house>
2023-02-17 10:59:29 -0300

    feat: add SignUp factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { SignUpController } from '../../presentation/controllers/sign-up/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validatorAdapter'
+import { DbAddAccount } from '../../data/usercases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const bCryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bCryptAdapter, accountMongoRepository)
+  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+}
